bddbd12a4b4c324bd0fbef1aa82fc36b0001a361
Ravi Verma <88143365+RaviVerma73@users.noreply.github.com>
2022-04-25 15:43:06 +0530

    Add files via upload

---
+++ contact-us.php
@@ -0,0 +1,103 @@
+<?php
+include('includes/config.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>News Portal | Contact us</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/modern-business.css" rel="stylesheet">
+    <style>
+      .map-container-3{
+  overflow:hidden;
+  padding-bottom:56.25%;
+  position:relative;
+  height:0;
+}
+.map-container-3 iframe{
+  left:0;
+  top:0;
+  height:50%;
+  width:50%;
+  position:absolute;
+}
+    </style>
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <?php include('includes/header.php');?>
+    <!-- Page Content -->
+    <div class="container">
+
+<?php 
+$pagetype='contactus';
+$query=mysqli_query($con,"select PageTitle,Description from tblpages where PageName='$pagetype'");
+while($row=mysqli_fetch_array($query))
+{
+
+?>
+      <h1 class="mt-4 mb-3"><?php echo htmlentities($row['PageTitle'])?>
+  
+      </h1>
+
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <a href="index.php">Home</a>
+        </li>
+        <li class="breadcrumb-item active">Contact</li>
+      </ol>
+
+      <!-- Intro Content -->
+      
+
+
+      <div class="row">
+
+        <div class="col-lg-12">
+
+          <p><?php echo $row['Description'];?></p>
+        </div>
+      </div>
+      <!-- /.row -->
+<?php } ?>
+    
+          <!-- map ifram -->
+
+        <!--Google map-->
+<div id="map-container-google-3" class="z-depth-1-half map-container-2">
+  <iframe src="https://goo.gl/maps/xBn1vqiM2G7QmKPk9" frameborder="0"
+    style="border:0" allowfullscreen></iframe>
+</div>
+
+      <!-- map ifram -->
+
+
+    </div>
+    <!-- /.container -->
+  
+
+    <!-- Footer -->
+ <?php include('includes/footer.php');?>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
